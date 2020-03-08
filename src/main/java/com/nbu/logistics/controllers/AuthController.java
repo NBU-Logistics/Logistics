66b37f2a19aa9bbdb3478ed5f9d7a0b223c432c2
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2020-03-08 19:06:52 +0200

    auth controller update

--- src/main/java/com/nbu/logistics/controllers/AuthController.java
+++ src/main/java/com/nbu/logistics/controllers/AuthController.java
@@ -5,6 +5,7 @@ import com.nbu.logistics.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AuthController {
@@ -12,8 +13,10 @@ public class AuthController {
     private AuthService authService;
 
     @RequestMapping("/login")
-    public String showLogin() {
-        return "login";
+    public ModelAndView showLogin(ModelAndView modelAndView) {
+        modelAndView.setViewName("login");
+
+        return modelAndView;
     }
 
     @RequestMapping("/register")
