a1cd1828565fdc6b7e55fccffebd0997796dee41
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2020-03-28 21:34:27 +0200

    fixed small database schema bug

--- src/main/java/com/nbu/logistics/entities/Office.java
+++ src/main/java/com/nbu/logistics/entities/Office.java
@@ -16,7 +16,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Entity
 @Where(clause = "is_deleted='false'")
-public class Office extends Company {
+public class Office {
     @Column
     private String name;
 }
